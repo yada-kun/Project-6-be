07f4aca94b4b0cca8ae6885d88afe5ec855f0fb5
James Anuncio <anunciojames14@gmail.com>
2022-07-29 00:11:47 +0800

    fix: part 2

--- index.js
+++ index.js
@@ -15,21 +15,19 @@ app.use(bodyParser.json());
 // Connection to the MongoDB Database
 const mongoose = require('mongoose');
 // mongoose.connect('mongodb://localhost:27017/project6');
-mongoose.connect(mongodb).then((response) => {
+mongoose.connect(mongodb);
+app.use(cors());
+
+app.get("/", (request, response) => {
+    response.send("Recieved Mesage");
+
 })
-app.use(cors())
-
-
 const NovelRouter = require('./routes/novels');
 app.use('/api/novels', NovelRouter );
 
 const AnnouncementRouter = require('./routes/announcements')
 app.use('/api/announcements', AnnouncementRouter );
 
-app.get("/", (request, response) => {
-    response.send("Recieved Mesage");
-
-})
 
 app.listen( port, () => {
     console.log(`Express App is live at port ${port}`);
